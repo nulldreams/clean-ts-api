fa3cf44b47e2f42c1964fd03dcd22e7d5912af82
nulldreams <nulldreams@gmail.com>
2021-02-03 03:24:20 -0300

    hotfix: env example and mongodb repo

--- src/main/routes/express/find-movie-by-id.ts
+++ src/main/routes/express/find-movie-by-id.ts
@@ -1,9 +1,9 @@
-import { PostgresMoviesRepository } from '@/infra/db/repositories'
+import { MongoMoviesRepository } from '@/infra/db/repositories'
 import { adaptRoute } from '@/main/adapters'
 import { findMovieByIdController } from '@/main/factories'
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  const repository = new PostgresMoviesRepository()
+  const repository = new MongoMoviesRepository()
   router.get('/movies/:movieId', adaptRoute(findMovieByIdController(repository)))
 }
